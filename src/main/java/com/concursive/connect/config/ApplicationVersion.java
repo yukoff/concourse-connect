974cdd307fea3405c1bb76f1d3464d9d5f585dba
matt <matt@0a69d750-447c-11de-a5f8-e1a32e2bd165>
2009-06-23 15:04:21 +0000

    CHANGE: Preparing for 1.0.2 release
    git-svn-id: http://concourseconnect.googlecode.com/svn/concourseconnect/trunk@133 0a69d750-447c-11de-a5f8-e1a32e2bd165

--- src/main/java/com/concursive/connect/config/ApplicationVersion.java
+++ src/main/java/com/concursive/connect/config/ApplicationVersion.java
@@ -56,9 +56,9 @@ package com.concursive.connect.config;
  */
 public class ApplicationVersion {
   public final static String TITLE = "ConcourseConnect";
-  public final static String VERSION = "1.0.1 (2009-06-02)";
-  public final static String APP_VERSION = "2009-06-02";
-  public final static String DB_VERSION = "2009-06-02";
+  public final static String VERSION = "1.0.2 (2009-06-23)";
+  public final static String APP_VERSION = "2009-06-23";
+  public final static String DB_VERSION = "2009-06-23";
 
 
   /**

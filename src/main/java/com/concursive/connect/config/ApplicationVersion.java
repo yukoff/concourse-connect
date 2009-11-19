992bffc85a058a74c82f6b4d9a9695eb3cec733c
matt <matt@0a69d750-447c-11de-a5f8-e1a32e2bd165>
2009-11-19 15:05:33 +0000

    CHANGE: to trigger the upgrade process to 2.0 Milestone 3
    git-svn-id: http://concourseconnect.googlecode.com/svn/concourseconnect/trunk@392 0a69d750-447c-11de-a5f8-e1a32e2bd165

--- src/main/java/com/concursive/connect/config/ApplicationVersion.java
+++ src/main/java/com/concursive/connect/config/ApplicationVersion.java
@@ -56,9 +56,9 @@ package com.concursive.connect.config;
  */
 public class ApplicationVersion {
   public final static String TITLE = "ConcourseConnect";
-  public final static String VERSION = "2.0 beta (2009-09-29)";
-  public final static String APP_VERSION = "2009-09-29";
-  public final static String DB_VERSION = "2009-09-29";
+  public final static String VERSION = "2.0 milestone 3 (2009-11-19)";
+  public final static String APP_VERSION = "2009-11-19";
+  public final static String DB_VERSION = "2009-11-19";
 
 
   /**

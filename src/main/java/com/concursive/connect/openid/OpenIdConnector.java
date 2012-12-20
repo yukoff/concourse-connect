67c5503b8b54d7f72ead449dfdcf54a2060025be
Yury Buldakov <yury.buldakov@portaone.com>
2012-12-20 13:17:18 +0200

    Modified for YesFX portal

---
+++ src/main/java/com/concursive/connect/openid/OpenIdConnector.java
@@ -0,0 +1,19 @@
+package com.concursive.connect.openid;
+
+import org.openid4java.discovery.Identifier;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.message.ax.FetchRequest;
+import org.openid4java.message.ax.FetchResponse;
+import org.openid4java.message.ax.AxMessage;
+import org.openid4java.message.*;
+import org.openid4java.OpenIDException;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.io.IOException;
+
+public class OpenIdConnector {
+
+}
